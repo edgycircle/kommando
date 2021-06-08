9806f9155935f248b2570af0867374e3d48408f9
David Strauß <david.strauss@edgycircle.com>
2021-06-08 15:42:09 +0200

    Fix deadlock in `Concurrent::FixedThreadPool` on older Ruby versions
    It looks like `@pool.wait_for_termination` causes a deadlock when the
    pool is not shutting down. This did not happen on Ruby 2.7, only in a
    Ruby 2.5 application.

--- lib/kommando/scheduled_command_worker.rb
+++ lib/kommando/scheduled_command_worker.rb
@@ -1,3 +1,5 @@
+require 'concurrent'
+
 module Kommando
   class ScheduledCommandWorker
     def initialize(adapter, dependencies, number_of_threads)
@@ -9,6 +11,11 @@ module Kommando
     def start
       @pool = Concurrent::FixedThreadPool.new(@number_of_threads)
       @number_of_threads.times { schedule_next_run(0) }
+
+      while !@pool.shutdown?
+        sleep 1
+      end
+
       @pool.wait_for_termination
     end
 
